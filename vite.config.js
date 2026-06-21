19e0e0a0ddaeddcf014551732a06827d7f49c192
Sania Batool <24sw01@quest.edu.pk>
2026-06-21 12:07:40 +0500

    Fix base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/My-Portfolio/', // 🔁 Must match your GitHub repo name
   plugins: [react(), tailwindcss()],
-})
+});
